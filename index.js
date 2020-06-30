b9263e7dfca6c87904d6dec617e28f2e779b4a61
KKJUSTKK <62921713+KKJUSTKK@users.noreply.github.com>
2020-06-30 23:15:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js);
 const bot = new discord.client();
-const token=('NzI3NTY5OTUwMjY0NTI0OTMw.Xvt3yQ.ah2bjRXwG3UfM26oHFM03PY6cPA');
+
 
 bot.on ('ready',()=> {
 console.log('im on baby')};
